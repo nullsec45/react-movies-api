5c482c48268a33e86677fc4fdbb6a3952feb8ab8
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-21 12:06:23 +0700

    index untuk components

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,7 @@
-import Modal from "./Modal";
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
 import Search from "./Search";
 
 export {
-    Modal, MovieList, Navbar, Search
+    MovieList, Navbar, Search
 }
