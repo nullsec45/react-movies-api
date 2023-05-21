9d7be503ee067352cd68ff4c6625245214f533ff
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-21 12:06:56 +0700

    Search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,16 +2,21 @@ import React, { Component } from 'react'
 
 export default class Search extends Component {
     render() {
+        const { handleSubmit, title, changeTitle } = this.props;
+
         return (
             <div className="row mt-3 justify-content-center" >
                 <div className="col-md-8">
                     <h1 className="text-center">Search Movie</h1>
-                    <div className="input-group mb-3">
-                        <input type="text" className="form-control" placeholder="Movie title" id="search-input" />
-                        <button className="btn btn-primary" type="button" id="search-button">Search</button>
-                    </div>
+                    <form action="">
+                        <div className="input-group mb-3">
+                            <input type="text" className="form-control" placeholder="Movie title" id="search-input" value={title} onChange={(event) => changeTitle(event)} />
+                            <button className="btn btn-primary" type="submit" id="search-button" onClick={handleSubmit}>Search</button>
+
+                        </div>
+                    </form>
                 </div>
-            </div>
+            </div >
         )
     }
 }
