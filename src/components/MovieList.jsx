f8836014b7ff3b46114f58fbe76c46c3b7c117af
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-05-21 12:06:35 +0700

    Movie List

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -2,19 +2,25 @@ import React, { Component } from 'react'
 
 export default class MovieList extends Component {
     render() {
+        const { movies } = this.props;
         return (
-            <div class="row mt-4" id="movie-list">
-                <div class="col-md-4">
-                    <div class="card mb-3">
-                        <img src="" class="card-img-top img-fluid" alt="" />
-                        <div class="card-body">
-                            <h5 class="card-title"></h5>
-                            <h6 class="card-subtitle mb-2 text-muted"></h6>
-                            <a href="#" class="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
+            <>
+                <div className="row mt-4" id="movie-list" >
+                    {movies && movies.map((movie) => (
+                        <div className="col-md-4" key={movie.imdbID}>
+                            <div className="card mb-3">
+                                <img src={movie.Poster} className="card-img-top img-fluid" alt={movie.Title} />
+                                <div className="card-body">
+                                    <h5 className="card-title">{movie.Title}</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">{movie.Year}</h6>
+                                    <a href={`/detail/${movie.imdbID}`} className="btn btn-primary see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="${data.imdbID}">See Detail</a>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
+                    ))
+                    }
+                </div >
+            </>
         )
     }
 }
